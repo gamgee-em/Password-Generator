b9b852ae99fcb8a9de93ddb430199111a2942b4e
Sam Sweigart <samuel.sweigart@gmail.com>
2021-03-18 09:32:59 -0400

    moved user character  choices into a single variable containing an array

--- scripts/script.js
+++ scripts/script.js
@@ -2,17 +2,22 @@
 
 // try making an array of password characters and access them by index in if statements
 // instead of multiple variables
-var upChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+/* var upChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowChars = "abcdefghijklmnopqrstuvwxyz";
 let numChars = "0123456789";
-let specChars = "!#$%?";
+let specChars = "!#$%?"; */
+
+let charArr = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "0123456789", "!#$%?"];
+
 let passArr = [];
 let password = [];
+let passCharStr = [];
 
 // need to fix prompt to activate on event click not on browser load.
 // added onclick event to html for now.
 // try figuring out a solution using addEventListener('click', function()).
 let generatePassword = () => {
+
   // ask user to pick password length
   let userLenChars = prompt("Choose a password length between 8 & 128 characters");
 
@@ -24,36 +29,43 @@ let generatePassword = () => {
   
   // try seperating else statement into independent if statement. Maybe this is why the
   // gaurd clause/statement is not exiting the function.
+/*   if (isNaN(userLenChars)) {
+    alert('must enter a number!');
+  } */
   if (userLenChars === null || userLenChars == '' || userLenChars < 8 || userLenChars > 128) {
     return userLenChars;
     //why is this statement continuing to run after returning false?
     //try else if statement 
   } else {
     if (userUpChars) {
-      password.push(upChars);
+      password.push(charArr[0]);
     } else {
+      // try to alert user instead of logging
       console.log('It\'d be safer if you did!')
     }
 
     if (userLowChars) {
-      password.push(lowChars);
+      password.push(charArr[1]);
     } else {
       console.log('It\'d be safer if you did!')
     }
 
     if (userNumChars) {
-      password.push(numChars);
+      password.push(charArr[2]);
     } else {
       console.log('It\'d be safer if you did!')
     }
 
     if (userSpecChars) {
-      password.push(specChars);
+      password.push(charArr[3]);
     } else {
       console.log('It\'d be safer if you did!')
     }
-  
+    console.log(passCharStr)
+    console.log(password)
     passCharStr = password.join("");
+    console.log(passCharStr)
+
   }
  
   let randPassArr = [];
@@ -66,8 +78,6 @@ let generatePassword = () => {
   }
 }
   return document.getElementById('password').innerHTML = randPassArr.join("");
-
-
 };
 
 /* 
@@ -78,3 +88,5 @@ let generatePassword = () => {
   When the browser is reloaded the function then works as expected for only the first execution.
   possible direction: find a way to reset function or reload browser?
 */
+
+console.log(window)
