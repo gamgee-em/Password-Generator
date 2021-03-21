1de8410d80d909160e0d1e4728063365126fe4a2
Sam Sweigart <samuel.sweigart@gmail.com>
2021-03-21 16:14:59 -0400

    changed #gen-header to update once password is generated

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,5 @@
-// Assignment Code
-
 // Character choices array
 let charArr = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz", "01234567890123456789", "!#>-$%?+&=@<_!#>-$%?+&=@<_"];
-
 // Add click event
 let displayPass = document.getElementById('generate');
 displayPass.addEventListener('click', generatePassword);
@@ -13,9 +10,11 @@ function generatePassword() {
   let password = [];
   let passCharStr = [];
   let regExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W\_])[A-Za-z\d\W\_]{8,128}$/;
+  let finalPass = document.getElementById('password');
+  let genHeader = document.getElementById('gen-header');
   // ask user to pick password length
   let userLenChars = prompt("Choose a password length between 8 & 128 characters");
-  //parse userLenChars input string to number for conditional statements
+  //parse userLenChars input string to number for use in conditional statements
   let parUserLenChars = parseInt(userLenChars);
 
   // function alerts user that their password would be safer if they included the extra chars
@@ -24,12 +23,12 @@ function generatePassword() {
   };
 
   // ask the user if they would like to include uppecase, lowercase, numerical and/or special characters.
-  // use user input from promts to generate password array
+  // use user input from prompts to generate password array
   if (isNaN(parUserLenChars) || parUserLenChars === null || parUserLenChars == '' || parUserLenChars < 8 || parUserLenChars > 128) {
       alert('Invalid Input. Please try again');
       return;
   }
-  
+
   let userUpChars = confirm("Would you like UPPERCASE characters?");
   userUpChars ? password.push(charArr[0]) : safePrompt();
   
@@ -38,29 +37,33 @@ function generatePassword() {
   
   let userNumChars = confirm("Would you like num3r1ca1 characters?");
   userNumChars ? password.push(charArr[2]) : safePrompt();
-
+  
   let userSpecChars = confirm("Would you like $pec!al characters?");
   userSpecChars ? password.push(charArr[3]) : safePrompt();
 
   // create new arrays and fill each with chosen password chars
   // convert to a string and replace all commas between arrarys with an
   // empty string
-    passCharStr = Array(userLenChars)
-    .fill(password)
-    .toString()
-    .replaceAll(',', '');
-
-  let finalPass = document.getElementById('password');
+  passCharStr = Array(userLenChars)
+  .fill(password)
+  .toString()
+  .replaceAll(',', '');
 
   // add random char from passCharStr to randPass until
   // randPass string length matches users desired password length
   do {
     randPass += passCharStr[Math.floor(Math.random() * passCharStr.length)];
-    
   } while (randPass.length < userLenChars);
 
+  // check if generated password includes all of the users desired characters
+  // if not return string asking user to try again.
+  randPass.match(regExp) ? finalPass : randPass = 'Password not secure. Please run again 😕';
+  genHeader.innerHTML = 'Your Secure Generated Password'
+  return finalPass.innerHTML = randPass;
+}
+
     /* 
-    ***fix the bug***
+    ***fix bug***
     Actual:
     When randPass doesn't include all of the requested password chars
     randPass is set to an empty string and ends the function. Causing 
@@ -69,14 +72,5 @@ function generatePassword() {
     Expected:
     If randPass doesn't include all of the requested password chars
     run function again (without prompting user for more input) until a password
-    including all requested characters is received.
-
+    including all requested characters is received
      */
-
-    // check if generated password includes all of the users desired characters
-    // if not return empty string.
-    randPass.match(regExp) ? finalPass : randPass = 'Password not secure. Please run again 😕';
-    
-    return finalPass.innerHTML = randPass;
-
-}
