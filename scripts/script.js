560066a734ad26aab6ce960e8de8179c7b8a712c
Sam Sweigart <samuel.sweigart@gmail.com>
2021-03-22 10:20:06 -0400

    took out last of comments & removed regexp

--- scripts/script.js
+++ scripts/script.js
@@ -9,7 +9,6 @@ function generatePassword() {
   let randPass = [];
   let password = [];
   let passCharStr = [];
-  let regExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W\_])[A-Za-z\d\W\_]{8,128}$/;
   let finalPass = document.getElementById('password');
   let genHeader = document.getElementById('gen-header');
   // ask user to pick password length
@@ -22,13 +21,14 @@ function generatePassword() {
     alert('It\'d be safer if you did!')
   };
 
-  // ask the user if they would like to include uppecase, lowercase, numerical and/or special characters.
-  // use user input from prompts to generate password array
-  if (isNaN(parUserLenChars) || parUserLenChars === null || parUserLenChars == '' || parUserLenChars < 8 || parUserLenChars > 128) {
+  // check userLenChars input for type and value range
+  if (isNaN(parUserLenChars) || parUserLenChars < 8 || parUserLenChars > 128) {
       alert('Invalid Input. Please try again');
       return;
   }
 
+  // ask the user if they would like to include uppecase, lowercase, numerical and/or special characters.
+  // use user input from prompts to generate password array
   let userUpChars = confirm("Would you like UPPERCASE characters?");
   userUpChars ? password.push(charArr[0]) : safePrompt();
   
@@ -41,7 +41,7 @@ function generatePassword() {
   let userSpecChars = confirm("Would you like $pec!al characters?");
   userSpecChars ? password.push(charArr[3]) : safePrompt();
 
-  // create new arrays and fill each with chosen password chars
+  // create new arrays and fill each with chosen password chars from password array
   // convert to a string and replace all commas between arrarys with an
   // empty string
   passCharStr = Array(userLenChars)
@@ -57,20 +57,6 @@ function generatePassword() {
 
   // check if generated password includes all of the users desired characters
   // if not return string asking user to try again.
-  randPass.match(regExp) ? finalPass : randPass = 'Password not secure. Please run again 😕';
-  genHeader.innerHTML = 'Your Secure Generated Password'
+  genHeader.innerHTML = 'Your Secure Generated Password';
   return finalPass.innerHTML = randPass;
 }
-
-    /* 
-    ***fix bug***
-    Actual:
-    When randPass doesn't include all of the requested password chars
-    randPass is set to an empty string and ends the function. Causing 
-    the user to have to rerun the application for desired results.
-
-    Expected:
-    If randPass doesn't include all of the requested password chars
-    run function again (without prompting user for more input) until a password
-    including all requested characters is received
-     */
